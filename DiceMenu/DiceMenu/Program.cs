aa9afca910df2977761908e3ae00130f2ba6db07
TomasG <tomijus7@gmail.com>
2020-02-10 15:56:09 +0200

    Project created

---
+++ DiceMenu/DiceMenu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiceMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
